fda409d16a15527eefcbb50f734a5b2ddcfc6f05
Jacquie Tran <jacquie.tran@gmail.com>
2022-05-16 20:53:18 +1200

    aussie pals

---
+++ R/noise_studies_0137.R
@@ -0,0 +1,103 @@
+# Load libraries ---------------------------------------------------------------
+
+library(dplyr)
+library(ggplot2)
+library(ambient)
+
+# Source custom function -------------------------------------------------------
+
+source(here::here("R/functions/weave_noise.R"))
+
+# Modifiable parameters --------------------------------------------------------
+
+iteration_id <- "noise_studies_0137"
+seed_num <- 789137
+initial_grid_size <- 30
+warp_factor <- 100 # lower = more warping
+line_colour <- "#BBACC1"
+bg_colour <- "#E7E3DA"
+
+colour_vec1 <- c(
+  paletteer::paletteer_d("ochRe::lorikeet")[1],
+  "#54783C",
+  paletteer::paletteer_d("ochRe::lorikeet")[6],
+  "#84A830",
+  paletteer::paletteer_d("ochRe::lorikeet")[5]) # 5 cols
+colour_vec2 <- c(
+  paletteer::paletteer_d("ochRe::lorikeet")[2],
+  paletteer::paletteer_d("ochRe::lorikeet")[3],
+  paletteer::paletteer_d("ochRe::lorikeet")[4])
+outline_colour <- "#BBACC1"
+
+# Make some noise --------------------------------------------------------------
+
+# Generate data
+grid <- weave_noise(
+  seed = seed_num, x_max = 2, y_max = 2, grid_length = initial_grid_size,
+  warp_factor = warp_factor, geom_size_min = 10, geom_size_max = 20,
+  noise_type = "value")
+
+# Add colours
+set.seed(seed_num)
+grid <- grid %>%
+  mutate(
+    colour1 = case_when(
+      y <= 0.25*max(grid$y)      ~ sample(
+          colour_vec1, n(), replace = TRUE,
+          prob = c(0.7, 0.15, 0.1, 0.025, 0.025)),
+      y <= 0.5*max(grid$y) &
+        y > 0.25*max(grid$y)     ~ sample(
+          colour_vec1, n(), replace = TRUE,
+          prob = c(0.5, 0.2, 0.15, 0.075, 0.075)),
+      y <= 0.75*max(grid$y) &
+        y > 0.5*max(grid$y)     ~ sample(
+          colour_vec1, n(), replace = TRUE,
+          prob = c(0.3, 0.25, 0.2, 0.125, 0.125)),
+      y > 0.75*max(grid$y)      ~ sample(
+          colour_vec1, n(), replace = TRUE,
+          prob = c(0.1, 0.3, 0.25, 0.175, 0.175))),
+    colour2 = sample(c(colour_vec2), n(), replace = TRUE))
+
+# Build plot -------------------------------------------------------------------
+
+ggplot() +
+  ggfx::as_reference(
+    geom_curve(
+      data = grid,
+      aes(x = x, y = y, xend = x_warped, yend = y_warped, size = size,
+          colour = colour1),
+      curvature = -0.2),
+    id = "base") +
+  geom_point(
+    data = grid %>% filter(subset >= 12),
+    aes(x = x_warped, y = y_warped, size = size / 3),
+    colour = bg_colour) +
+  ggfx::with_blend(
+    geom_point(
+      data = grid %>% filter(subset < 3),
+      aes(x = x_warped, y = y_warped, size = size * 1.2, colour = colour2),
+      shape = 16),
+    bg_layer = "base",
+    blend_type = "overlay") +
+  geom_point(
+    data = grid %>% filter(subset < 10 & subset >= 7),
+    aes(x = x_warped, y = y_warped, size = size * 0.4),
+    colour = line_colour, shape = 5) +
+  geom_path(
+    data = grid %>% filter(subset >= 12),
+    aes(x = x_warped, y = y_warped),
+    colour = bg_colour, size = 0.4, linetype = "dotted") +
+  scale_size_identity() +
+  scale_colour_identity() +
+  coord_equal(expand = FALSE) +
+  theme_void() +
+  theme(
+    legend.position = "none",
+    plot.background = element_rect(fill = bg_colour, colour = bg_colour),
+    plot.margin     = margin(140,140,140,140, unit = "pt"))
+
+# Export to file ---------------------------------------------------------------
+
+ggsave(
+  here::here(glue::glue("img/{`iteration_id`}.png")),
+  device = "png", width = 6000, height = 6000, units = "px", dpi = 600)
---
+++ R/noise_studies_0150.R
@@ -0,0 +1,81 @@
+# Load libraries ---------------------------------------------------------------
+
+library(dplyr)
+library(ggplot2)
+library(ambient)
+
+# Source custom function -------------------------------------------------------
+
+source(here::here("R/functions/weave_noise.R"))
+
+# Modifiable parameters --------------------------------------------------------
+
+iteration_id <- "noise_studies_0150"
+seed_num <- 568150
+initial_grid_size <- 200
+warp_factor <- 70 # lower = more warping
+colour_vec1 <- rev(c("#AB7E37", "#BD852C", "#79222A", "#B9ACA3", "#768B99")) # 5 cols
+bg_colour <- "#E8EDDF"
+
+# Make some noise --------------------------------------------------------------
+
+# Generate data
+grid <- weave_noise(
+  seed = seed_num, x_max = 1, y_max = 1.5, grid_length = initial_grid_size,
+  warp_factor = warp_factor, geom_size_min = 0.5, geom_size_max = 3,
+  noise_type = "cubic")
+
+# Add colours
+set.seed(seed_num)
+grid <- grid %>%
+  mutate(
+    colour = case_when(
+      y <= 0.25*max(grid$y)      ~ sample(
+        colour_vec1, n(), replace = TRUE,
+        prob = c(0.7, 0.15, 0.1, 0.025, 0.025)),
+      y <= 0.5*max(grid$y) &
+        y > 0.25*max(grid$y)     ~ sample(
+          colour_vec1, n(), replace = TRUE,
+          prob = c(0.5, 0.2, 0.15, 0.075, 0.075)),
+      y <= 0.75*max(grid$y) &
+        y > 0.5*max(grid$y)     ~ sample(
+          colour_vec1, n(), replace = TRUE,
+          prob = c(0.3, 0.25, 0.2, 0.125, 0.125)),
+      y > 0.75*max(grid$y)      ~ sample(
+        colour_vec1, n(), replace = TRUE,
+        prob = c(0.1, 0.3, 0.25, 0.175, 0.175))))
+
+# Build plot -------------------------------------------------------------------
+
+ggplot() +
+  geom_curve(
+    data = grid,
+    aes(x = x, y = y, xend = x_warped, yend = y_warped, size = size,
+        colour = colour),
+    curvature = -0.2) +
+  geom_point(
+    data = grid %>% filter(subset >= 2),
+    aes(x = x_warped, y = y_warped, size = size / 3),
+    colour = bg_colour) +
+  geom_point(
+    data = grid %>% filter(subset < 10 & subset >= 6),
+    aes(x = x_warped, y = y_warped, size = size / 2, colour = colour),
+    shape = 0) +
+  geom_point(
+    data = grid %>% filter(subset < 6),
+    aes(x = x_warped, y = y_warped, size = size / 1.5, colour = colour),
+    shape = 15) +
+  scale_size_identity() +
+  scale_colour_identity() +
+  coord_equal(expand = TRUE) +
+  theme_void() +
+  theme(
+    legend.position = "none",
+    plot.background = element_rect(fill = bg_colour, colour = bg_colour),
+    plot.margin     = margin(30,30,30,30, unit = "pt"))
+
+# Export to file ---------------------------------------------------------------
+
+ggsave(
+  here::here(glue::glue("img/{`iteration_id`}.png")),
+  device = "png", width = 4000, height = 6000, units = "px", dpi = 600)
